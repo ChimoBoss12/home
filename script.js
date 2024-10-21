410f9d885430d8a65601a62bde602c9fa3d542f8
ChimbroDaPro <102317600+ChimoBoss12@users.noreply.github.com>
2024-10-21 20:51:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-document.querySelectorAll('a').forEach(link => {
-  link.setAttribute('target', '_blank');
-});
-
-
 anime({
     targets: 'h1, h2',
     translateY: [-50, 0],
     opacity: [0, 1],
-    duration: 3000,
+    duration: 1000,
     easing: 'easeOutExpo',
-    delay: anime.stagger(600)  // Delays each text element
+    delay: function(el, i) {
+        return 2500 + anime.stagger(200); // 1000ms initial delay, then 200ms stagger delay
+      }
 });
 document.querySelectorAll('button').forEach(button => {
     button.addEventListener('mouseover', () => {
         anime({
             targets: button,
             scale: 1.2,
-            duration: 1500,
+            duration: 500,
             easing: 'easeInOutQuad'
         });
     });
@@ -25,7 +22,7 @@ document.querySelectorAll('button').forEach(button => {
         anime({
             targets: button,
             scale: 1,
-            duration: 1500,
+            duration: 500,
             easing: 'easeInOutQuad'
         });
     });
@@ -72,6 +69,33 @@ anime({
     targets: 'section',
     opacity: [0, 1],
     translateY: [20, 0],
-    delay: anime.stagger(600),
+    delay: function(el, i) {
+        return 2500 + anime.stagger(200); // 1000ms initial delay, then 200ms stagger delay
+      },
     easing: 'easeOutQuad'
 });
+anime({
+  targets: '.now',
+    opacity: [1, 1],
+  duration: 1200, 
+  easing: 'easeInOutQuad',
+  complete: function() {
+    const nowElement = document.querySelector('.now');
+    nowElement.classList.add('hidden');
+      const notyetElement = document.querySelector('.notyet');
+      notyetElement.classList.remove('hidden');
+  }
+});
+
+setTimeout(function() {
+    const nowElement = document.querySelector('.now');
+    const notyetElement = document.querySelector('.notyet');
+    const top = document.getElementById('top');
+    const bottom = document.getElementById('bottom');
+
+    document.body.style.overflow = 'auto';
+    nowElement.classList.add('hidden');
+    notyetElement.classList.add('hidden');
+    top.classList.add('hidden');
+    bottom.classList.add('hidden');
+}, 3300);
